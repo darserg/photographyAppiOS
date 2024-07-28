f66484cb87fdfdd81e11de01e1ea669991e53658
Darserg <Darserg1212>
2024-07-28 16:08:23 +0400

    Initial Commit

---
+++ photographyApp/photographyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  photographyAppApp.swift
+//  photographyApp
+//
+//  Created by Сергей Дарьин on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct photographyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
