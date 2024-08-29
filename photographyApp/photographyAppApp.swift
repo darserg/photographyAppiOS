b7f0251075cdc6bc1e82b076f4aa7c0691a8e69a
Darserg <Darserg1212>
2024-08-29 16:23:34 +0400

    Add FireBase into app

--- photographyApp/photographyAppApp.swift
+++ photographyApp/photographyAppApp.swift
@@ -6,9 +6,19 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    private func application(_ application: UIApplicationDelegate, didFinishLaunchingWithOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct photographyAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
